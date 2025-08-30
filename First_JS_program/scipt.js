7b09727dcdf9482c65197f12a9ce494340ee3ae9
akadamboy <abhijith.mampara@gmail.com>
2025-08-30 20:39:15 +0530

    type of variables

--- First_JS_program/scipt.js
+++ First_JS_program/scipt.js
@@ -4,7 +4,28 @@ if (js === 'amazing') alert('java script is fun');
 sum = 40 + 8 +23 -10;
 console.log(sum);
 
-let firstName = "Tony"
-let lastName = "Stark"
+let firstName = "Tony";
+let lastName = "Stark";
 
-alert(firstName+" "+lastName)
+alert(firstName+" "+lastName);
+
+//----------------------------------------------//
+//------ DATA TYPES --------//
+
+//JavaScript has Dynamic Typing
+// we dont need to explicitly define the data type 
+// java script automatically understands the variable type
+
+let age = 23  // age becomes integer
+console.log(typeof(age))
+
+let age1 = '23' // here age becomes a String
+console.log(typeof(age1))
+
+let winner = true // winner is of type boolean
+console.log(typeof(winner))
+
+let items; //here it is undefined since we didnt give any value yet
+console.log(typeof(items))
+
+// there are other types Null, Symbol, BingInt
