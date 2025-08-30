bfb85d15e923277013f500b69b8b5ead0407e4d2
akadamboy <abhijith.mampara@gmail.com>
2025-08-30 21:01:00 +0530

    more about variable declaration and let, const and var

--- First_JS_program/scipt.js
+++ First_JS_program/scipt.js
@@ -8,24 +8,3 @@ let firstName = "Tony";
 let lastName = "Stark";
 
 alert(firstName+" "+lastName);
-
-//----------------------------------------------//
-//------ DATA TYPES --------//
-
-//JavaScript has Dynamic Typing
-// we dont need to explicitly define the data type 
-// java script automatically understands the variable type
-
-let age = 23  // age becomes integer
-console.log(typeof(age))
-
-let age1 = '23' // here age becomes a String
-console.log(typeof(age1))
-
-let winner = true // winner is of type boolean
-console.log(typeof(winner))
-
-let items; //here it is undefined since we didnt give any value yet
-console.log(typeof(items))
-
-// there are other types Null, Symbol, BingInt
