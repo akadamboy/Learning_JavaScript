6e05fe4d0ad6a4c95472947e68eacb3956c0b573
akadamboy <abhijith.mampara@gmail.com>
2025-08-30 15:10:07 +0530

    writing first script using js

---
+++ First_JS_program/scipt.js
@@ -0,0 +1,10 @@
+alert('Hello World!');
+let js = 'amazing';
+if (js === 'amazing') alert('java script is fun');
+sum = 40 + 8 +23 -10;
+console.log(sum);
+
+let firstName = "Tony"
+let lastName = "Stark"
+
+alert(firstName+" "+lastName)
