348b29301d4fd75f8f05887dc49589f0cdab6fee
akadamboy <abhijith.mampara@gmail.com>
2025-09-03 19:35:39 +0530

    done formating

--- JavaScript_Fundamentals_1/type_converision.js
+++ JavaScript_Fundamentals_1/type_converision.js
@@ -4,31 +4,27 @@
 
 //--- CONVERTING STRING TO NUMBER ---//
 
-const birthYear = '2000'; //string
+const birthYear = "2000"; //string
 
-console.log(birthYear + 18 ); // out put willbe 200018
+console.log(birthYear + 18); // out put willbe 200018
 
 // string + number will append the number at the end of string
 
-console.log(Number(2000) + 18 ); // correct way to add string plus number
+console.log(Number(2000) + 18); // correct way to add string plus number
 
 //only work if the string contains actuall number
 
-const name = 'dude';
+const name = "dude";
 
 console.log(Number(name)); // we get out put -> Nan (Not a Number)
 console.log(typeof NaN); // type of Nan is number since its represent invalid number
 
-
 //--- CONVERTING NUMBER TO STRING
 
 const age = 23;
-console.log(String(age) + ' is your age');
-
-
+console.log(String(age) + " is your age");
 
 //-- CONVERT TO BOOLEANS --//
-
 
 // TRUTHY values and FALSY values //
 
@@ -37,14 +33,14 @@ console.log(String(age) + ' is your age');
 // Truthy values convert to TRUTH when we convert them to BOOLEAN
 
 // 5 Falsy values
-    // 0
-    // '' -> enpty string
-    // undefined
-    // null
-    // NaN
-console.log('Falsy values example');
+// 0
+// '' -> enpty string
+// undefined
+// null
+// NaN
+console.log("Falsy values example");
 console.log(Boolean(0));
-console.log(Boolean(''));
+console.log(Boolean(""));
 console.log(Boolean(undefined));
 console.log(Boolean(null));
 console.log(Boolean(NaN));
@@ -52,41 +48,24 @@ let price; // price is now undefined converting to boolean will return false
 console.log(Boolean(price));
 // all of the above will be falsei
 
-console.log('truthy value example');
+console.log("truthy value example");
 
 console.log(Boolean(5));
-console.log(Boolean('not empty string'));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(Boolean("not empty string"));
 
 //----- TYPE COERSION -----//
 
-console.log('i am ' +23+ 'year old'); // convert 23 to string
+console.log("i am " + 23 + "year old"); // convert 23 to string
 
-console.log('23' + '10' + 5); // + operator convert all the numbers given to string
+console.log("23" + "10" + 5); // + operator convert all the numbers given to string
 
-console.log('23' - '10' - 3); // - operator convert all the strings to numbers
+console.log("23" - "10" - 3); // - operator convert all the strings to numbers
 
-console.log('23' * 10); // * convert strings to numbers
+console.log("23" * 10); // * convert strings to numbers
 
-console.log('230'/10); // divide also convert strings to numbers
+console.log("230" / 10); // divide also convert strings to numbers
 //------------------------------------------------------------------------//
 
-let n = '1' + 1; // n becomes 11 since + convert number to string
+let n = "1" + 1; // n becomes 11 since + convert number to string
 n = n - 1; // n becomes 10 ( 11 - 1 ) since - convert string 11 to number 11
 console.log(n); // output will be 10
-
--- JavaScript_Fundamentals_1/variables.js
+++ JavaScript_Fundamentals_1/variables.js
@@ -1,20 +1,20 @@
 //----------------------------------------------//
 //------ DATA TYPES --------//
 //JavaScript has Dynamic Typing
-// we dont need to explicitly define the data type 
+// we dont need to explicitly define the data type
 // java script automatically understands the variable type
 
-let age = 23 ; // age becomes integer
-console.log(typeof(age));
+let age = 23; // age becomes integer
+console.log(typeof age);
 
-let age1 = '23' // here age becomes a String
-console.log(typeof(age1));
+let age1 = "23"; // here age becomes a String
+console.log(typeof age1);
 
-let winner = true // winner is of type boolean
-console.log(typeof(winner));
+let winner = true; // winner is of type boolean
+console.log(typeof winner);
 
 let items; //here it is undefined since we didnt give any value yet
-console.log(typeof(items));
+console.log(typeof items);
 
 // console.log(typeof 23); not neccessary to add brackets after typeof
 
@@ -22,9 +22,9 @@ console.log(typeof(items));
 
 // console.log(javaScriptIsFun) --> this will not work since java script is interpreted language
 let javaScriptIsFun = true;
-if(javaScriptIsFun === true) alert("java script is fun");
+if (javaScriptIsFun === true) alert("java script is fun");
 
-javaScriptIsFun = 23 //we can change the data type by assigning a different value of different type
+javaScriptIsFun = 23; //we can change the data type by assigning a different value of different type
 
 let year;
 
@@ -33,8 +33,7 @@ console.log(year); // this will return undefined since no value is assigned
 //--------------------------------------------------------//
 //--- let, var, const ----//
 
-let x,y; // we can define 2 variables like this
-
+let x, y; // we can define 2 variables like this
 
 let ageOfStudent = 30;
 ageOfStudent = 31;
@@ -48,14 +47,13 @@ const birthYear = 2000;
 
 //  const birthYear; --> this will also cause error. Alwayse assigne value while using const.
 
-// we will get the error "missing initializer in const declaration" 
-
+// we will get the error "missing initializer in const declaration"
 
 //---var------//
 // not recommanted to use.
 
-var job = 'consultant'
-job = 234
+var job = "consultant";
+job = 234;
 
 //let and var are similar but it will allow you to change the value though out function
 // but let is block scoped. if we use let in for and if loops it will only work in that loop we can do anything out thise those blocks
