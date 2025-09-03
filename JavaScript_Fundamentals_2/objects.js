98f468fcbe80adc98dbfba9338ba85e199bf5b4d
akadamboy <abhijith.mampara@gmail.com>
2025-09-03 15:31:10 +0530

    creating functions inside objects

--- JavaScript_Fundamentals_2/objects.js
+++ JavaScript_Fundamentals_2/objects.js
@@ -32,6 +32,47 @@ console.log( `person have ${person.friends.length+1} friends and their names are
 )
 
 
+//----------------------------------------------------------------//
+
+//we can function expression as a value in object//
+
+
+const person2 ={
+    name : 'sam',
+    birthyear : 2001,    
+    job: 'consultant',
+    friends:  ['john', 'alwin', 'ray', 'sam',],
+
+    getAge : function (birthYear){
+        return 2025 - birthYear
+    },
+
+    leaglAge : function (){
+        if(2025 - this.birthyear >= 18){        //this  keyword takes value if the value is present in the object itself
+            return "you are of legal age"
+        }else{
+            return "you are not of legal age"
+        }
+    },
+
+    findAgeAndStore : function (){
+        this.age = 2025 - this.birthyear
+    }
+}
+
+console.log(person2.getAge(person2.birthyear));
+
+console.log(person2['getAge'](person2.birthyear));
+
+console.log(person2.leaglAge); // this displays funciton code as it is
+console.log(person2.leaglAge()); // this executes the code
+
+person2.findAgeAndStore()
+console.log(person2)  // this will have a new object key value pair age
+
+
+
+
 
 
 
