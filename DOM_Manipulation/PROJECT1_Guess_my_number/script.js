60d06940cdfa15178d51be0d95cd65e64a3d8759
akadamboy <abhijith.mampara@gmail.com>
2025-09-04 14:33:14 +0530

    created logic for game over

--- DOM_Manipulation/PROJECT1_Guess_my_number/script.js
+++ DOM_Manipulation/PROJECT1_Guess_my_number/script.js
@@ -1,14 +1,14 @@
 "use strict";
 
-const myNumber = Math.trunc(Math.random() * 21);
+const myNumber = Math.trunc(Math.random() * 20) + 1;
 
-console.log(document.querySelector(".message").textContent);
+// console.log(document.querySelector(".message").textContent);
 
 let score = Number(document.querySelector(".score").textContent);
 let highScore = document.querySelector(".highscore").textContent;
-console.log(document.querySelector(".guess").value);
+// console.log(document.querySelector(".guess").value);
 
-console.log(`score is ${score}`);
+// console.log(`score is ${score}`);
 // console.log(`highscore is ${highScore}`);
 
 // document.querySelector(".number").textContent = 13;
@@ -23,31 +23,47 @@ const resetButton = document.querySelector(".reset");
 
 checkButton.addEventListener("click", function () {
   const numberEntered = Number(document.querySelector(".guess").value);
-  if (numberEntered <= 20) {
-    if (numberEntered === myNumber) {
-      document.querySelector(".number").textContent = myNumber;
-      document.querySelector(".message").textContent = "🎉 Correct Number!";
-      document.querySelector(".score").textContent = score;
-      if (score > highScore) {
-        highScore = score;
-        document.querySelector(".highscore").textContent = score;
-      }
-    } else if (numberEntered < myNumber) {
-      document.querySelector(".message").textContent = "Guess Heigher";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "Guess Lower";
-      score--;
+  //   console.log(numberEntered);
+  if (!numberEntered) {
+    document.querySelector(".message").textContent = "Please enter a number";
+  } else if (numberEntered > 20) {
+    document.querySelector(".message").textContent =
+      "Number > 20, only 1 to 20 allowed";
+  } else if (numberEntered === myNumber) {
+    document.querySelector(".number").textContent = myNumber;
+    document.querySelector(".message").textContent = "🎉 Correct Number!";
+    document.querySelector(".score").textContent = score;
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = score;
+    }
+  } else if (numberEntered < myNumber) {
+    document.querySelector(".message").textContent = "Guess Heigher";
+    score--;
+    if (score === 0) {
+      score = 20;
+      document.querySelector(".number").textContent = "?";
+      document.querySelector(".guess").value = "";
+      document.querySelector(".message").textContent = "Game Over";
       document.querySelector(".score").textContent = score;
     }
+    document.querySelector(".score").textContent = score;
   } else {
-    document.querySelector(".message").textContent =
-      "Enter Number Between 1 and 20";
+    document.querySelector(".message").textContent = "Guess Lower";
+    score--;
+    if (score === 0) {
+      score = 20;
+      document.querySelector(".number").textContent = "?";
+      document.querySelector(".guess").value = "";
+      document.querySelector(".message").textContent = "Game Over";
+      document.querySelector(".score").textContent = score;
+    }
+
+    document.querySelector(".score").textContent = score;
   }
 });
 
-resetButton.addEventListener("click", function () {
+resetButton.addEventListener("click", function reset() {
   score = 20;
   document.querySelector(".number").textContent = "?";
   document.querySelector(".guess").value = "";
