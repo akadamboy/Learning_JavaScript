59cad23fa2ff315a04a3aebb237febf308373e7f
akadamboy <abhijith.mampara@gmail.com>
2025-09-06 23:36:26 +0530

    learned about keyboard events

---
+++ DOM_Manipulation/PROJECT2_Modal_window/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const showbuttonElement = document.querySelectorAll(".show-modal");
+const modalElement = document.querySelector(".modal");
+const overlayElement = document.querySelector(".overlay");
+const closeButtonElement = document.querySelector(".close-modal");
+
+function closeModal() {
+  modalElement.classList.add("hidden");
+  overlayElement.classList.add("hidden");
+}
+
+function openModal() {
+  modalElement.classList.remove("hidden");
+  overlayElement.classList.remove("hidden");
+}
+
+showbuttonElement.forEach((btn) => btn.addEventListener("click", openModal));
+
+closeButtonElement.addEventListener("click", closeModal);
+
+overlayElement.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (event) {
+  console.log(event.key);
+  if (event.key === "Escape") closeModal();
+});
