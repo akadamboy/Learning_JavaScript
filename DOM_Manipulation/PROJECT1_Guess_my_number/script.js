9154fa4d2dbd3a7de58903f402365d8a50810bec
akadamboy <abhijith.mampara@gmail.com>
2025-09-06 21:10:42 +0530

    completed the gussing game

--- DOM_Manipulation/PROJECT1_Guess_my_number/script.js
+++ DOM_Manipulation/PROJECT1_Guess_my_number/script.js
@@ -11,6 +11,7 @@ const resetButton = document.querySelector(".reset");
 const inputElement = document.querySelector(".guess");
 const messageElement = document.querySelector(".message");
 const secretNumber = document.querySelector(".number");
+const bodyElement = document.querySelector("body");
 
 let score = Number(scoreElement.textContent);
 let highScore = Number(highScoreElement.textContent);
@@ -36,42 +37,47 @@ function GameOver() {
 // const check_button = document.getElementsByClassName("check")[0];
 
 checkButton.addEventListener("click", function () {
-  const numberEntered = Number(inputElement.value);
-  //   console.log(numberEntered);
-  if (!numberEntered) {
+  if (inputElement.value === "") {
     messageElement.textContent = "Please enter a number";
-  } else if (numberEntered > 20 || numberEntered < 1) {
-    messageElement.textContent = "Number > 20, only 1 to 20 allowed";
-  } else if (numberEntered === myNumber) {
-    inputElement.disabled = true;
-    checkButton.disabled = true;
-    secretNumber.textContent = myNumber;
-    messageElement.textContent = "🎉 Correct Number!";
-    scoreElement.textContent = score;
-    if (score > highScore) {
-      highScore = score;
-      highScoreElement.textContent = score;
-    }
-  } else if (numberEntered < myNumber) {
-    messageElement.textContent = "Guess Heigher";
-    score--;
-    if (score === 0) {
-      GameOver();
-    }
-    scoreElement.textContent = score;
   } else {
-    messageElement.textContent = "Guess Lower";
-    score--;
-    if (score === 0) {
-      GameOver();
-    }
+    const numberEntered = Number(inputElement.value);
+    if (numberEntered > 20 || numberEntered < 1) {
+      messageElement.textContent = "Number > 20, only 1 to 20 allowed";
+    } else if (numberEntered === myNumber) {
+      bodyElement.style.backgroundColor = "#60b347";
+      secretNumber.style.width = "30rem";
+      inputElement.disabled = true;
+      checkButton.disabled = true;
+      secretNumber.textContent = myNumber;
+      messageElement.textContent = "🎉 Correct Number!";
+      scoreElement.textContent = score;
+      if (score > highScore) {
+        highScore = score;
+        highScoreElement.textContent = score;
+      }
+    } else if (numberEntered < myNumber) {
+      messageElement.textContent = "Guess Heigher";
+      score--;
+      if (score === 0) {
+        GameOver();
+      }
+      scoreElement.textContent = score;
+    } else {
+      messageElement.textContent = "Guess Lower";
+      score--;
+      if (score === 0) {
+        GameOver();
+      }
 
-    scoreElement.textContent = score;
+      scoreElement.textContent = score;
+    }
   }
 });
 
 resetButton.addEventListener("click", function reset() {
   myNumber = Math.trunc(Math.random() * 20) + 1;
+  bodyElement.style.backgroundColor = "#222";
+  secretNumber.style.width = "15rem";
   score = 20;
   inputElement.disabled = false;
   checkButton.disabled = false;
